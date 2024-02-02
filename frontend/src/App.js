2a2087672b811c24a6357ed87a7f3837807350f7
Shubham <bhattacharya.shubham123@gmail.com>
2024-02-01 22:13:23 -0600

    Navbar jsx done and basic scss also done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
+import NavBar from "./components/navbar/navbar";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>This is the start of the website!</h1>
+        {/* <h1>This is the start of the website!</h1> */}
       </header>
+      < NavBar />
     </div>
   );
 }
--- frontend/src/components/navbar/navbar.jsx
+++ frontend/src/components/navbar/navbar.jsx
@@ -1,11 +1,26 @@
 import React from "react";
+import './navbar.scss'
 
 const NavBar = () => {
   return (
-    <div>
-      <h1>Hello Testing</h1>
+    <div className="navbar">
+      <div className="logo">
+        <img src="frontend/src/components/navbar/empty-logo-for-swqap-recipe.jpg" alt="Logo" />
+      </div>
+      <div className="search-bar">
+        <input type="text" placeholder="Search" />
+        <button className="search-button">
+          <span role="img" aria-label="Search">
+            🔍
+          </span>
+        </button>
+      </div>
+      <div className="buttons">
+        <button>About Us</button>
+        <button>Login</button>
+      </div>
     </div>
   );
-}
+};
 
 export default NavBar;
